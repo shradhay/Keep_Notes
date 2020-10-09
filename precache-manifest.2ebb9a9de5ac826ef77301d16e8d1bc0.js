61734854df45c527f5d9b51321b6958e95f23680
shradhay <shraddhayannawar16@gmail.com>
2020-10-09 00:50:29 -0700

    Updates

---
+++ precache-manifest.2ebb9a9de5ac826ef77301d16e8d1bc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9b2d5d4bb029cdee1c3d69e714047b",
+    "url": "/Keep_Notes/index.html"
+  },
+  {
+    "revision": "b8f1704a778735ef7efc",
+    "url": "/Keep_Notes/static/css/main.e6035ffd.chunk.css"
+  },
+  {
+    "revision": "060de9ad2f58567afc31",
+    "url": "/Keep_Notes/static/js/2.9b829dac.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Keep_Notes/static/js/2.9b829dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8f1704a778735ef7efc",
+    "url": "/Keep_Notes/static/js/main.7a96596c.chunk.js"
+  },
+  {
+    "revision": "e8ba5b90c28b70e8a295",
+    "url": "/Keep_Notes/static/js/runtime-main.897317df.js"
+  }
+]);
